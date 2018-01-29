64c11ec3319484679b4cec4016ce80d80dc54680
Ayush Malviya <ayushmalviya27@gmail.com>
2018-01-29 22:11:27 +0530

    2nd

--- snakeGame.py
+++ snakeGame.py
@@ -17,5 +17,35 @@ else:
 playSurface = pygame.display.set_mode((720, 460))
 pygame.display.set_caption('Snake Game!')
 
+#Colors
 
 red = pygame.Color(255,0,0)
+green = pygame.Color(0,255,0)
+black = pygame.Color(0,0,0)
+white = pygame.Color(255,255,255)
+brown = pygame.Color(165,42,42)
+
+#FPS Controller (look into detail)
+fpsController = pygame.time.Clock()
+
+snakePos = [100,50]
+snakeBody = [[100,50],[90,50],[80,50]]
+
+foodPos = [random.randrange(1,72)*10,random.randrange(1,46)*10]
+foodSpawn = True
+
+direction = 'RIGHT'
+changeto = direction
+
+#Game over function
+
+def gameOver():
+	myFont = pygame.font.SysFont('monaco', 72)
+	GOsurf = myFont.render('Game Over!', True, red)
+	GOrect = GOsurf.get_rect()
+	GOrect.midtop = (360, 15)
+	playSurface.blit(GOsurf,GOrect) ##This too
+	pygame.display.flip()  ##Discuss this in detail
+
+gameOver()
+time.sleep(10)
