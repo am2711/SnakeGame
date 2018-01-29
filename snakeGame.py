c59762daf052095ec484ac42686e77da6cb89817
Ayush Malviya <ayushmalviya27@gmail.com>
2018-01-29 22:55:52 +0530

    Directions

--- snakeGame.py
+++ snakeGame.py
@@ -81,7 +81,16 @@ while True:
 		direction = 'DOWN'
 
 
+	if direction == 'RIGHT':
+		snakePos[0] += 10
+	if direction == 'LEFT':
+		snakePos[0] -= 10
+	if direction == 'UP':
+		snakePos[1] -= 10
+	if direction == 'DOWN':
+		snakePos[1] += 10
 
+	
 
 
 
