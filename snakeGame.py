02a2b36f7d58cd1cd9507b9ea9b305b0c273e0ea
Ayush Malviya <ayushmalviya27@gmail.com>
2018-03-01 18:25:16 +0530

    Before push

--- snakeGame.py
+++ snakeGame.py
@@ -46,10 +46,12 @@ def gameOver():
 	GOrect.midtop = (360, 15)
 	playSurface.blit(GOsurf,GOrect) ##This too
 	pygame.display.flip()  ##Discuss this in detail
-	time.sleep()
+	time.sleep(1	)
 	pygame.quit()	#pygame exit
 	sys.exit() 	#console exit
 
+	def showScore():
+		sFont = pygame.font.SysFont('monaco' 24)
 
 # Main Logic
 
@@ -100,7 +102,27 @@ while True:
 	if foodSpawn == False:
 		foodPos = [random.randrange(1,72)*10,random.randrange(1,46)*10]
 	foodSpawn = True
-		
+
+
+	playSurface.fill(white)
+	for pos in snakeBody:
+
+		pygame.draw.rect(playSurface, green, 
+		pygame.Rect(pos[0],pos[1],10,10) )
+
+		pygame.draw.rect(playSurface, brown	, 
+		pygame.Rect(foodPos[0],foodPos[1],10,10) )
+
+		if snakePos[0] > 710 or snakePos[0] < 0:
+			gameOver() 
+		if snakePos[1] > 450 or snakePos[1] < 0:
+			gameOver()
+
+		for block in snakeBody[1:]:
+			if snakePos[0] == block[0] and snakePos[1] == block[1]:
+				gameOver()
+	pygame.display.flip()
+	fpsController.tick(25)
 
 
 
